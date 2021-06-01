7334a97cbcad96472a70fc66a09ef791582b47a2
Timur <timur@anonshop.ru>
2021-06-01 18:45:40 +0300

    готовы задачи без звёздочки к уроку 5

--- classes/Img.php
+++ classes/Img.php
@@ -22,5 +22,30 @@ class Img{
             echo "Нет изображений в галерее";
         }
     }
+
+    public function getGaleryFromBase(){
+        $images = db::getDB("SELECT * FROM `images`");
+        if ($images->num_rows > 0){
+            foreach($images as $image){
+                echo "<a href=\"?id=" . $image['images_id'] . "\" target=\"_blank\">";
+                echo "  <img src=\"" . $image['images_url'] . "\" alt=\"" . $image['images_name'] . "\" class=\"galery-item\">";
+                echo "</a>";
+            };
+        }else{
+            echo "Нет изображений в галерее";
+        }
+    }
+
+    public function getImgById($id){
+        $images = db::getDB("SELECT * FROM `images` WHERE `images_id` = $id");
+        if ($images->num_rows > 0){
+            echo "<a href=\".\" class=\"navigation-back\">назад</a>";
+            foreach($images as $image){
+                echo "  <img src=\"" . $image['images_url'] . "\" alt=\"" . $image['images_name'] . "\" class=\"galery-item galery-item-big\">";
+            };
+        }else{
+            echo "Что-то пошло не так";
+        }
+    }
 }
 ?>
--- scripts/dowload.php
+++ scripts/dowload.php
@@ -1,4 +1,5 @@
 <?php
+include_once "../classes/db.php";
 session_start();
 $fileSize = ($_POST['size'] > 0) ? $_POST['size'] : 10; # размер мегабайт
 
@@ -6,8 +7,15 @@ if (isset($_FILES['file']) && $_FILES['file']['error'] == 0){
     if ($_FILES['file']['size'] > 1024 * 1024 * $fileSize){
         $_SESSION['error'] = "Вес файла превышает $fileSize мб.";
     }elseif (preg_match("/.+(\.jpg)/",$_FILES['file']['name'])){
-        $_SESSION['good'] = "Файл добавлен в галерею.";
+        $name = preg_replace("/\.jpg/", "", $_FILES['file']['name']);
+        $images = db::getDB("SELECT * FROM `images` WHERE `images_name` LIKE '$name'");
+        if ($images->num_rows == 0){
         move_uploaded_file($_FILES['file']['tmp_name'], "../lesson4/img/" . $_FILES['file']['name']);
+        db::getDB("INSERT INTO `images`(`images_name`, `images_url`) VALUES ('$name','img/{$_FILES['file']['name']}')");
+        $_SESSION['good'] = "Файл добавлен в галерею.";
+    }else{
+        $_SESSION['error'] = "Файл уже был загружен ранее.";
+    }
     }else{
         $_SESSION['error'] = "Не верный формат файла.";
     }
