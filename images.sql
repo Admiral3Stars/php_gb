41bfcab2af8fd31f0d2b1178e09807273973cde7
Timur <timur@anonshop.ru>
2021-06-02 12:27:39 +0300

    обновлен дамп базы

--- images.sql
+++ images.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3307
--- Время создания: Июн 01 2021 г., 18:47
+-- Время создания: Июн 02 2021 г., 12:27
 -- Версия сервера: 8.0.19
 -- Версия PHP: 8.0.1
 
@@ -30,8 +30,17 @@ SET time_zone = "+00:00";
 CREATE TABLE `images` (
   `images_id` int NOT NULL,
   `images_name` varchar(255) NOT NULL,
-  `images_url` varchar(255) NOT NULL
+  `images_url` varchar(255) NOT NULL,
+  `images_count` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `images`
+--
+
+INSERT INTO `images` (`images_id`, `images_name`, `images_url`, `images_count`) VALUES
+(1, 'Ll5b50C0wmg', 'img/Ll5b50C0wmg.jpg', 12),
+(2, 'DSC_3976tr', 'img/DSC_3976tr.jpg', 15);
 
 --
 -- Индексы сохранённых таблиц
@@ -51,7 +60,7 @@ ALTER TABLE `images`
 -- AUTO_INCREMENT для таблицы `images`
 --
 ALTER TABLE `images`
-  MODIFY `images_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `images_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
